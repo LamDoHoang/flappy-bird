46c013897fcf91cfaab16789931b3f12e1d9cc06
LamDoHoang <irokatsuna@gmail.com>
2022-03-08 17:50:56 +0700

    fix bug

--- Assets/Scripts/FlyingBird.cs
+++ Assets/Scripts/FlyingBird.cs
@@ -18,7 +18,7 @@ public class FlyingBird : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * velocity;
             SfxManager.sfxInstance.Audio.PlayOneShot(SfxManager.sfxInstance.Click);
